f2d10b034bd6be709e6a1cd88e5b856809d9bf56
Leandro Ferreira <durang@gmail.com>
2014-04-16 11:33:12 -0300

    added bower integration

---
+++ bower.js
@@ -0,0 +1,17 @@
+{
+	"name": "flowplayer",
+	"version": "5.4.6",
+	"main": [
+		"lib/flowplayer.js"
+	],
+	"dependencies": {
+		"jquery.browser": "dist/jquery.browser.js",
+		"branding": "dist/branding.min.js"
+	},
+	"ignore": [
+		"node_modules/",
+		"components/",
+		"website/",
+		"libs/"
+	]
+}
